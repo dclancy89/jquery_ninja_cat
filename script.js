4ac299cf3b7ad233968df5f4f2e744d8b3674c27
Daniel <clancy.daniel@gmail.com>
2018-02-08 10:46:42 -0600

    validated html and added comments to script

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 
 $(document).ready(function() {
 
+
+	// swap the images on click
 	$('img').click(function() {
+
+		//sets src into temp
 		var temp = $(this).attr('src')
-		console.log(temp);
+
+		//swaps the values
 		$(this).attr('src', $(this).attr('data-alt-src'));
 		$(this).attr('data-alt-src', temp);
 	});
